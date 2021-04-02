2ede66ca90a273606804113b5b9fadf032470bf7
micamarietta <micamarietta@gmail.com>
2021-04-02 16:00:33 -0700

    Last NPC adds, decor, etc
    Last changes

---
+++ New Unity Project/Assets/Creator Kit - RPG/Scripts/Gameplay/EndofGame.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEditor.SceneManagement;
+
+
+public class EndofGame : MonoBehaviour
+{
+
+    public GameObject Quest;
+    
+}
