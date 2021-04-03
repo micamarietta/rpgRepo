bcd1359525fdc3c5dac8aceedaf6a367738852e0
micamarietta <micamarietta@gmail.com>
2021-04-02 17:00:50 -0700

    Fixed NPC, Leave Game button and script added
    Fixed chest NPC
    Leave Game option added

---
+++ New Unity Project/Assets/Creator Kit - RPG/Scripts/Gameplay/leaveGame.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leaveGame : MonoBehaviour
+{
+    public void OnButtonPress()
+    {
+        Application.Quit();
+    }
+
+}
